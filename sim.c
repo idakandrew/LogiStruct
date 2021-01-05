4e7ee3f08e5559109f2ebdb963bf257db88b6a3d
Andrew Idak <75957379+idakandrew@users.noreply.github.com>
2021-01-05 17:25:11 -0500

    Initial commit.

---
+++ sim.c
@@ -0,0 +1,57 @@
+#include "canvas.h"
+
+void chip_sim(int map[96][50]) {
+    for(int i = 0; i < 96; i++) {
+        for(int j = 0; j < 50; j++) {
+            if(map[i][j] == and) {
+                if(map[i-3][j-1] == hipinin && map[i-3][j+1] == hipinin) {
+                    map[i+3][j] = hipinout;
+                } else if(map[i-3][j] != hipinin && map[i-3][+1] != hipinin) {
+                    map[i+3][j] = lopinout;
+                }
+            } else if(map[i][j] == not) {
+                if(map[i-3][j] == lopinin) {
+                    map[i+3][j] = hipinout;
+                } else if(map[i-3][j] == hipinin) {
+                    map[i+3][j] = lopinout;
+                }
+            }
+        }
+    }
+}
+
+void wire_sim(int map[96][50], int x, int y, int mode) {
+    if(!mode) {
+        for(int i = 0; i < 96; i++) {
+            for(int j = 0; j < 50; j++) {
+                if(map[i][j] == hiwire) {
+                    map[i][j] = lowire;
+                } else if(map[i][j] == hilight) {
+                    map[i][j] = lolight;
+                } else if(map[i][j] == hipinin) {
+                    map[i][j] = lopinin;
+                }
+            }
+        }
+
+        for(int i = 0; i < 96; i++) {
+            for(int j = 0; j < 50; j++) {
+                if(map[i][j] == hipinout || map[i][j] == hiflip) {
+                    if(map[i+1][j] == lowire || map[i+1][j] == lolight || map[i+1][j] == lopinin) {wire_sim(map, i+1, j, 1);}
+                    if(map[i-1][j] == lowire || map[i-1][j] == lolight || map[i-1][j] == lopinin) {wire_sim(map, i-1, j, 1);}
+                    if(map[i][j+1] == lowire || map[i][j+1] == lolight || map[i][j+1] == lopinin) {wire_sim(map, i, j+1, 1);}
+                    if(map[i][j-1] == lowire || map[i][j-1] == lolight || map[i][j-1] == lopinin) {wire_sim(map, i, j-1, 1);}
+                }
+            }
+        }
+        
+    } else {
+        if(map[x][y] == lowire) {map[x][y] = hiwire;}
+        else if(map[x][y] == lolight) {map[x][y] = hilight;}
+        else if(map[x][y] == lopinin) {map[x][y] = hipinin;}
+        if(map[x+1][y] == lowire || map[x+1][y] == lolight || map[x+1][y] == lopinin) {wire_sim(map, x+1, y, 1);}
+        if(map[x-1][y] == lowire || map[x-1][y] == lolight || map[x-1][y] == lopinin) {wire_sim(map, x-1, y, 1);}
+        if(map[x][y+1] == lowire || map[x][y+1] == lolight || map[x][y+1] == lopinin) {wire_sim(map, x, y+1, 1);}
+        if(map[x][y-1] == lowire || map[x][y-1] == lolight || map[x][y-1] == lopinin) {wire_sim(map, x, y-1, 1);}
+    }
+}
---
+++ sim.h
@@ -0,0 +1,8 @@
+#ifndef SIM
+#define SIM
+
+void chip_sim(int map[96][50]);
+
+void wire_sim(int map[96][50], int x, int y, int mode);
+
+#endif
