d4a9c2041c3b2180563f8f7c92d689ee14a70df5
Andrew Idak <75957379+idakandrew@users.noreply.github.com>
2021-01-16 15:34:58 -0500

    Implemented basic save functionality

--- utils.c
+++ utils.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <libloaderapi.h>
 
@@ -9,3 +10,21 @@ void fix_dir(void) {
     chdir(path); 
 }
 
+void save_canvas(int map[96][50]) {
+    FILE *fp = fopen("data/save.bin", "wb");
+    if(fp != NULL) {
+        fseek(fp, 0, SEEK_SET);
+        fwrite(map, sizeof(int)*96*50, 1, fp);
+        fclose(fp);
+    }
+}
+
+void load_canvas(int map[95][50]) {
+    FILE *fp = fopen("data/save.bin", "rb");
+    if(fp != NULL) {
+        fread(map, sizeof(int)*96*50, 1, fp);
+        fclose(fp);
+    } else {
+        memset(map, 0, sizeof(int)*96*50);
+    }
+}
--- utils.h
+++ utils.h
@@ -3,4 +3,8 @@
 
 void fix_dir(void);
 
+void save_canvas(int map[96][50]);
+
+void load_canvas(int map[96][50]);
+
 #endif
